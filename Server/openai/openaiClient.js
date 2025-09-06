4ce4d23af302ed74a431edca17115615670d8480
AbdulWasaySaleem <153143400+AbdulWasaySaleem@users.noreply.github.com>
2025-09-06 14:39:45 +0500

    .

--- Server/openai/openaiClient.js
+++ Server/openai/openaiClient.js
@@ -2,6 +2,8 @@ import OpenAI from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("process.env.OPENAI_API_KEY", process.env.OPENAI_API_KEY);
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
